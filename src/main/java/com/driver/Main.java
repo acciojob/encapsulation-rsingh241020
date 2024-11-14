a8d7e5b5afd2f363bfe9e50c0b72bc7078e0683f
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2024-11-15 04:54:34 +0530

    encapsulation assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+
+        // Try Setting a value to name by directly accessing it using obj and else try printin it
+
+        //rwOnly.name = "Rohit";
+        //'name' has private access in 'com.driver.RWOnly'
+
+        rwOnly.setName("Rohit");
+        System.out.println(rwOnly.getName());
+    }
 }
